e2b3cefbe00d714e8bb442fd93b87788b40321f1
Thwarikh Anvar T S <48545060+thwarikh@users.noreply.github.com>
2019-09-30 09:43:58 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+## [AnyDLBot](https://telegram.dog/AnyDLBot)
+---
+
+**There are multiple things I can do**:
+
+👉 All Supported Video Formats of https://rg3.github.io/youtube-dl/supportedsites.html
+
+👉 Upload as file from any HTTP link
+
+👉 Convert To Streamable Video, any Telegram media
+
+👉 ReName Telegram files, with custom thumbnail support
+
+👉 Get High Speed Direct Download Link of any Telegram file
+
+### Installation
+
+#### The Easiest Way
+
+**upgrade** your subscription for [@AnyDL_Bot](https://telegram.dog/AnyDl_Bot) without having to run anything on your own
+
+#### The Easy Way
+
+[![](http://ouo.io/images/banners/r1.jpg "ouo.io - Make short links and earn the biggest money")](http://ouo.io/ref/ROXeyb0X)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+#### The Hard Way
+
+```sh
+virtualenv -p python3 VENV
+. ./VENV/bin/activate
+pip install -r requirements.txt
+cp sample_config.py config.py
+--- EDIT config.py values appropriately ---
+python bot.py
+```
+
+- For FeedBack and Suggestions, please feel free to say in [@SpEcHlDe](https://telegram.dog/ShrimadhaVahdamirhS)
+
+#### LICENSE
+- GPLv3
---
+++ sample_config.py
@@ -0,0 +1,41 @@
+import os
+
+class Config(object):
+    # get a token from https://chatbase.com
+    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    # your domain to show when download file is greater than MAX_FILE_SIZE
+    HTTP_DOMAIN = os.environ.get("HTTP_DOMAIN", "https://example.com/")
+    # for running on Heroku.com
+    PORT = int(os.environ.get('PORT', 5000))
+    # The Telegram API things
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
+    # Get these values from my.telegram.org
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
+    # reg: Procedures
+    UTUBE_BOT_USERS = AUTH_USERS
+    SUPER_DLBOT_USERS = AUTH_USERS
+    SUPER3X_DLBOT_USERS = AUTH_USERS
+    SUPER7X_DLBOT_USERS = AUTH_USERS
+    BANNED_USERS = [
+        "7351948"
+    ]
+    # Wat was I thinking? :\
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 1572864000
+    FREE_USER_MAX_FILE_SIZE = 50000000
+    # chunk size that should be used with requests
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    # https://t.me/hevcbay/951
+    OUO_IO_API_KEY = ""
